59b54b97e25dc16505af3b26576d38d23ddb32a8
Ricardo SIGNES <rjbs@cpan.org>
2014-09-01 12:41:02 +0000

    Import of RJBS/Email-Valid-1.195 from CPAN.
    gitpan-cpan-distribution: Email-Valid
    gitpan-cpan-version:      1.195
    gitpan-cpan-path:         RJBS/Email-Valid-1.195.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Email/Valid.pm
+++ lib/Email/Valid.pm
@@ -2,7 +2,7 @@ require 5.006;
 use strict;
 use warnings;
 package Email::Valid;
-$Email::Valid::VERSION = '1.194';
+$Email::Valid::VERSION = '1.195';
 # ABSTRACT: Check validity of Internet email addresses
 our (
   $RFC822PAT,
@@ -146,7 +146,7 @@ sub _net_dns_query {
 
   my $packet = $Resolver->send($host, 'MX') or croak $Resolver->errorstring;
   if ($packet->header->ancount) {
-    my @mx_entries = grep { $_->{'type'} eq 'MX' } $packet->answer;
+    my @mx_entries = grep { $_->type eq 'MX' } $packet->answer;
     if(@mx_entries) {
         my $mx = ($mx_entries[0])->exchange;
         if ($mx eq '.' or $mx eq '') {
@@ -719,7 +719,7 @@ Email::Valid - Check validity of Internet email addresses
 
 =head1 VERSION
 
-version 1.194
+version 1.195
 
 =head1 SYNOPSIS
 
